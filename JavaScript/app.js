a558c0d9d61dfa119f4993314b84b5dbebf37677
Pierpaolo <plucarelli.94@gmail.com>
2016-03-21 13:29:31 +0000

    Added jQuery animations, Made navbar fixed-top

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,4 +1,51 @@
 $(document).ready(function(){
-	$("#datepicker").datepicker();
-	$("#ui-datepicker-div").addClass("ll-skin-melon");
+		var selected;
+
+		var getClasses = function(){
+			$("#heading-classes").text("Your classes for " + (selected === "Today" ? selected : "the " + selected));
+
+			//animate the classes divs
+			$(".module-info").each(function(i){
+
+				setTimeout(function(){
+					$(".module-info").eq(i).addClass("animate");
+				}, 150 * (i+1));
+			});
+		}
+
+		$("#datepicker").datepicker({
+						inline: true,
+            showOtherMonths: true,
+            dayNamesMin: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+		});
+		selected = $("#datepicker").val();
+
+		// stores the selected date;
+		$("#datepicker").on("change",function(){
+			$(".module-info").each(function(){
+				$(this).removeClass("animate");
+			});
+        	selected = $(this).val();
+    	});
+
+		$("#search-btn").click(function(){
+			// show the bottom container
+			$(".bottom-container").css("display", "block");
+			$(".bottom-container").css("height", "600px");
+
+			$('html, body').animate({
+          		scrollTop: 200
+        	}, 1200);
+
+			// -------------------------------------------
+			// make the call to google calnedar here
+			// then based on number of results recieved
+			// add skeleton classes to the module-info
+			// -------------------------------------------
+
+			// display th edate on screen
+			getClasses();
+		});
+
+
 });
