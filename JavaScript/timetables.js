9bebd17073b458be6f85da61e8711fb802211604
simmeon1 <simmeonn1@gmail.com>
2016-03-21 17:18:53 +0000

    ONY WORKS WITH cs IN SEARCH BOX. Added search functionality using hard-coded data.

---
+++ JavaScript/timetables.js
@@ -0,0 +1,44 @@
+var timetables = {
+	cs: {
+		monday: [{
+			room: "N117", startTime: "09:00", endTime: "10:00", 
+			module: "CM2015", description: "Object Oriented Software Design", type: "Lecture"
+		},
+		{
+			room: "N106/N118", startTime: "10:00", endTime: "11:00", 
+			module: "CM2015", description: "Object Oriented Software Design", type: "Tutorial"
+		}],
+		tuesday: [{
+			room: "N533", startTime: "09:00", endTime: "12:00", 
+			module: "CM2015", description: "Object Oriented Software Design", type: "Lab"
+		}],
+		wednesday: [{
+			room: "N118", startTime: "09:00", endTime: "10:00", 
+			module: "CM2027", description: "Software Architecture and Software Engineering", type: "Lecture"
+		},
+		{
+			room: "H999", startTime: "10:00", endTime: "11:00", 
+			module: "CM2027", description: "Software Architecture and Software Engineering", type: "Tutorial"
+		},
+		{
+			room: "N533", startTime: "12:00", endTime: "13:00", 
+			module: "CM2027", description: "Software Architecture and Software Engineering", type: "Lab"	
+		}],
+		thursday: [{
+			room: "N999", startTime: "15:00", endTime: "17:00", 
+			module: "CM2521", description: "Introduction to Data Networks", type: "Lecture"
+		}],
+		friday: [{
+			room: "N424/N523", startTime: "09:00", endTime: "11:00", 
+			module: "CM2521", description: "Introduction to Data Networks", type: "Lab"
+		},
+		{
+			room: "H234", startTime: "11:00", endTime: "12:00", 
+			module: "CM2003", description: "Dynamic Web Programming", type: "Lecture"
+		},
+		{
+			room: "N533", startTime: "14:00", endTime: "17:00", 
+			module: "CM2003", description: "Dynamic Web Programming", type: "Tutorial"	
+		}]
+	}
+}
