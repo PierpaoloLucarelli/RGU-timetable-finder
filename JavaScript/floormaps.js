c2065e5a9b976231c7b7d08c53339354a268354a
Thanos Sivridis <athsivridis@gmail.com>
2016-04-03 00:21:13 +0100

    Adding floormaps js for testing. POST Coursework deadline (My branch)

---
+++ JavaScript/floormaps.js
@@ -0,0 +1,543 @@
+/**
+ * Created by Jehutty on 02/04/2016.
+ */
+
+
+var RGU = {
+    RSE: {
+        level_1: {
+            rooms: ["N303"]
+        }
+    }
+};
+    // },
+    // HFOSC : {
+    //     level_1: {
+    //         rooms : []
+    //     }
+    // }
+
+
+
+
+
+// var N303 = "yeaahh";
+//     function draw(){
+//     var canvas = document.getElementById("canvas");
+//     var ctx = canvas.getContext("2d");
+//     ctx.save();
+//     ctx.beginPath();
+//     ctx.moveTo(80.3, 89.8);
+//     ctx.bezierCurveTo(80.3, 108.5, 65.2, 123.6, 46.5, 123.6);
+//     ctx.bezierCurveTo(27.9, 123.6, 12.8, 108.5, 12.8, 89.8);
+//     ctx.bezierCurveTo(12.8, 71.2, 27.9, 56.1, 46.5, 56.1);
+//     ctx.bezierCurveTo(65.2, 56.1, 80.3, 71.2, 80.3, 89.8);
+//     ctx.closePath();
+//     ctx.fillStyle = "rgb(162, 0, 147)";
+//     ctx.fill();
+//     ctx.lineWidth = 0.8;
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(76.8, 104.7);
+//     ctx.lineTo(99.4, 132.7);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(99.3, 132.7);
+//     ctx.lineTo(91.3, 139.3);
+//     ctx.bezierCurveTo(91.3, 139.3, 112.4, 172.4, 175.5, 164.4);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(23.4, 65.2);
+//     ctx.lineTo(0.3, 38.4);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(0.3, 38.4);
+//     ctx.lineTo(78.9, 25.1);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(79.1, 25.1);
+//     ctx.lineTo(69.3, 41.5);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(69.3, 41.5);
+//     ctx.lineTo(88.3, 64.2);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(88.3, 64.2);
+//     ctx.lineTo(98.0, 56.1);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(109.5, 70.2);
+//     ctx.lineTo(76.9, 29.2);
+//     ctx.lineTo(76.9, 29.2);
+//     ctx.lineTo(88.7, 8.8);
+//     ctx.lineTo(126.2, 56.9);
+//     ctx.lineTo(109.5, 70.2);
+//     ctx.closePath();
+//     ctx.stroke();
+//
+//     // layer1/Rectangle
+//     ctx.beginPath();
+//     ctx.moveTo(126.8, 92.6);
+//     ctx.lineTo(109.4, 70.6);
+//     ctx.lineTo(126.1, 57.4);
+//     ctx.lineTo(143.6, 79.3);
+//     ctx.lineTo(126.8, 92.6);
+//     ctx.closePath();
+//     ctx.stroke();
+//
+//     // layer1/Group
+//
+//     // layer1/Group/Path
+//     ctx.save();
+//     ctx.beginPath();
+//     ctx.moveTo(166.2, 106.2);
+//     ctx.lineTo(162.3, 86.8);
+//     ctx.bezierCurveTo(162.3, 86.8, 148.7, 88.2, 143.6, 79.3);
+//     ctx.lineTo(126.8, 92.6);
+//     ctx.bezierCurveTo(126.8, 92.6, 141.8, 110.2, 166.2, 106.2);
+//     ctx.closePath();
+//     ctx.stroke();
+//
+//     // layer1/Group
+//     ctx.restore();
+//
+//     // layer1/Group/Path
+//     ctx.save();
+//     ctx.beginPath();
+//     ctx.moveTo(162.3, 86.8);
+//     ctx.bezierCurveTo(162.3, 86.8, 171.6, 86.0, 176.3, 80.9);
+//     ctx.bezierCurveTo(176.4, 80.8, 176.6, 80.8, 176.8, 81.0);
+//     ctx.lineTo(188.2, 95.7);
+//     ctx.lineTo(184.9, 98.6);
+//     ctx.lineTo(193.9, 109.2);
+//     ctx.bezierCurveTo(193.9, 109.2, 187.5, 116.4, 178.3, 118.4);
+//     ctx.lineTo(172.8, 103.7);
+//     ctx.bezierCurveTo(172.8, 103.7, 170.4, 105.8, 166.2, 106.2);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.restore();
+//     ctx.beginPath();
+//     ctx.moveTo(127.4, 110.3);
+//     ctx.bezierCurveTo(127.4, 110.3, 121.3, 109.7, 116.4, 101.0);
+//     ctx.lineTo(126.8, 92.6);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(127.4, 110.3);
+//     ctx.lineTo(135.2, 99.6);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(147.2, 105.1);
+//     ctx.lineTo(141.2, 119.4);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(156.0, 106.8);
+//     ctx.lineTo(155.8, 121.9);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(140.6, 102.6);
+//     ctx.lineTo(136.8, 109.3);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(130.8, 105.6);
+//     ctx.bezierCurveTo(130.8, 105.6, 135.6, 110.3, 144.2, 112.3);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(132.4, 113.9);
+//     ctx.bezierCurveTo(132.4, 113.9, 143.6, 128.1, 178.3, 118.4);
+//     ctx.stroke();
+//
+//     // layer1/Group
+//
+//     // layer1/Group/Path
+//     ctx.save();
+//     ctx.beginPath();
+//     ctx.moveTo(109.1, 110.8);
+//     ctx.lineTo(116.0, 119.2);
+//     ctx.stroke();
+//
+//     // layer1/Group/Path
+//     ctx.beginPath();
+//     ctx.moveTo(116.0, 118.9);
+//     ctx.lineTo(108.0, 125.9);
+//     ctx.bezierCurveTo(108.0, 125.9, 131.8, 153.1, 173.0, 140.6);
+//     ctx.stroke();
+//
+//     // layer1/Rectangle
+//     ctx.restore();
+//     ctx.beginPath();
+//     ctx.moveTo(210.9, 162.7);
+//     ctx.lineTo(176.1, 166.4);
+//     ctx.lineTo(172.3, 130.6);
+//     ctx.lineTo(207.1, 126.9);
+//     ctx.lineTo(210.9, 162.7);
+//     ctx.closePath();
+//     ctx.stroke();
+//
+//     // layer1/Rectangle
+//     ctx.beginPath();
+//     ctx.moveTo(244.7, 158.9);
+//     ctx.lineTo(211.0, 162.5);
+//     ctx.lineTo(207.2, 127.0);
+//     ctx.lineTo(240.9, 123.4);
+//     ctx.lineTo(244.7, 158.9);
+//     ctx.closePath();
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(263.9, 156.8);
+//     ctx.lineTo(244.6, 158.8);
+//     ctx.lineTo(240.8, 123.5);
+//     ctx.lineTo(278.4, 119.5);
+//     ctx.lineTo(263.9, 156.8);
+//     ctx.closePath();
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(202.4, 121.8);
+//     ctx.lineTo(272.5, 115.1);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(288.9, 95.8);
+//     ctx.lineTo(278.4, 119.5);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(200.9, 115.1);
+//     ctx.lineTo(207.2, 114.4);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(207.2, 114.4);
+//     ctx.lineTo(207.2, 109.7);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(207.2, 109.7);
+//     ctx.lineTo(277.4, 52.1);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(277.4, 52.1);
+//     ctx.lineTo(281.0, 52.9);
+//     ctx.bezierCurveTo(281.0, 52.9, 254.9, 70.2, 280.4, 97.6);
+//     ctx.lineTo(272.5, 115.1);
+//     ctx.stroke();
+//
+//     // layer1/Group
+//
+//     // layer1/Group/Path
+//     ctx.save();
+//     ctx.beginPath();
+//     ctx.moveTo(296.3, 59.4);
+//     ctx.lineTo(287.0, 54.9);
+//     ctx.bezierCurveTo(287.0, 54.9, 266.2, 68.2, 281.0, 89.6);
+//     ctx.lineTo(296.3, 59.4);
+//     ctx.stroke();
+//
+//     // layer1/Group
+//     ctx.restore();
+//
+//     // layer1/Group/Path
+//     ctx.save();
+//     ctx.beginPath();
+//     ctx.moveTo(296.3, 59.4);
+//     ctx.lineTo(324.4, 73.2);
+//     ctx.bezierCurveTo(324.4, 73.2, 325.3, 80.9, 319.8, 90.6);
+//     ctx.lineTo(288.4, 75.0);
+//     ctx.lineTo(296.3, 59.4);
+//     ctx.closePath();
+//     ctx.stroke();
+//
+//     // layer1/Group
+//     ctx.restore();
+//
+//     // layer1/Group/Path
+//     ctx.save();
+//     ctx.beginPath();
+//     ctx.moveTo(288.4, 75.0);
+//     ctx.lineTo(281.0, 89.6);
+//     ctx.bezierCurveTo(283.5, 93.6, 289.3, 95.4, 289.3, 95.4);
+//     ctx.lineTo(287.3, 99.5);
+//     ctx.bezierCurveTo(287.3, 99.5, 310.6, 105.7, 319.8, 90.6);
+//     ctx.lineTo(288.4, 75.0);
+//     ctx.stroke();
+//
+//     // layer1/Rectangle
+//     ctx.restore();
+//     ctx.beginPath();
+//     ctx.moveTo(201.5, 99.0);
+//     ctx.lineTo(191.8, 106.9);
+//     ctx.lineTo(185.0, 98.4);
+//     ctx.lineTo(194.6, 90.6);
+//     ctx.lineTo(201.5, 99.0);
+//     ctx.closePath();
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(209.4, 96.8);
+//     ctx.lineTo(206.0, 99.6);
+//     ctx.lineTo(203.9, 97.0);
+//     ctx.lineTo(201.4, 99.1);
+//     ctx.lineTo(194.5, 90.5);
+//     ctx.lineTo(200.4, 85.8);
+//     ctx.lineTo(209.4, 96.8);
+//     ctx.closePath();
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(324.4, 73.2);
+//     ctx.lineTo(326.9, 67.9);
+//     ctx.bezierCurveTo(326.9, 67.9, 323.8, 56.1, 312.0, 48.9);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(312.0, 48.9);
+//     ctx.lineTo(316.4, 40.4);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(312.0, 17.6);
+//     ctx.lineTo(322.8, 21.9);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(200.4, 85.8);
+//     ctx.lineTo(212.2, 76.3);
+//     ctx.stroke();
+//
+//     // layer1/Group
+//
+//     // layer1/Group/Path
+//     ctx.save();
+//     ctx.beginPath();
+//     ctx.moveTo(235.5, 75.0);
+//     ctx.lineTo(215.0, 91.6);
+//     ctx.lineTo(210.7, 86.3);
+//     ctx.lineTo(216.6, 81.5);
+//     ctx.lineTo(212.2, 76.3);
+//     ctx.lineTo(226.9, 64.4);
+//     ctx.lineTo(235.5, 75.0);
+//     ctx.closePath();
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.restore();
+//     ctx.beginPath();
+//     ctx.moveTo(216.6, 81.5);
+//     ctx.lineTo(224.9, 74.7);
+//     ctx.stroke();
+//
+//     // layer1/Rectangle
+//     ctx.beginPath();
+//     ctx.moveTo(239.9, 71.7);
+//     ctx.lineTo(231.0, 61.1);
+//     ctx.lineTo(239.6, 53.8);
+//     ctx.lineTo(248.5, 64.5);
+//     ctx.lineTo(239.9, 71.7);
+//     ctx.closePath();
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(226.9, 64.4);
+//     ctx.lineTo(231.0, 61.1);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(239.6, 53.8);
+//     ctx.lineTo(226.9, 38.9);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.beginPath();
+//     ctx.moveTo(226.9, 38.9);
+//     ctx.lineTo(232.4, 34.7);
+//     ctx.stroke();
+//
+//     // layer1/Group
+//
+//     // layer1/Group/Path
+//     ctx.save();
+//     ctx.beginPath();
+//     ctx.moveTo(232.4, 33.8);
+//     ctx.lineTo(245.0, 49.9);
+//     ctx.bezierCurveTo(245.0, 49.9, 262.2, 33.7, 273.6, 28.2);
+//     ctx.lineTo(264.1, 9.0);
+//     ctx.bezierCurveTo(264.1, 9.0, 253.9, 11.4, 232.4, 33.8);
+//     ctx.closePath();
+//     ctx.stroke();
+//
+//     // layer1/Group
+//     ctx.restore();
+//
+//     // layer1/Group/Path
+//     ctx.save();
+//     ctx.beginPath();
+//     ctx.moveTo(273.6, 28.2);
+//     ctx.lineTo(264.1, 9.0);
+//     ctx.bezierCurveTo(264.1, 9.0, 286.3, -5.2, 315.5, 2.8);
+//     ctx.lineTo(310.1, 23.7);
+//     ctx.bezierCurveTo(310.1, 23.7, 292.4, 18.5, 273.6, 28.2);
+//     ctx.closePath();
+//     ctx.stroke();
+//
+//     // layer1/Group/Path
+//     ctx.beginPath();
+//     ctx.moveTo(291.9, 22.7);
+//     ctx.lineTo(291.5, 7.0);
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.restore();
+//     ctx.beginPath();
+//     ctx.moveTo(353.3, 37.7);
+//     ctx.lineTo(322.8, 22.4);
+//     ctx.lineTo(333.6, 0.8);
+//     ctx.lineTo(360.3, 14.2);
+//     ctx.lineTo(357.1, 20.5);
+//     ctx.lineTo(360.9, 22.5);
+//     ctx.lineTo(353.3, 37.7);
+//     ctx.closePath();
+//     ctx.stroke();
+//
+//     // layer1/Rectangle
+//     ctx.beginPath();
+//     ctx.moveTo(362.7, 42.3);
+//     ctx.lineTo(353.3, 37.7);
+//     ctx.lineTo(360.9, 22.4);
+//     ctx.lineTo(370.3, 27.1);
+//     ctx.lineTo(362.7, 42.3);
+//     ctx.closePath();
+//     ctx.stroke();
+//
+//     // layer1/Rectangle
+//     ctx.beginPath();
+//     ctx.moveTo(374.7, 54.1);
+//     ctx.lineTo(360.5, 46.9);
+//     ctx.lineTo(370.3, 27.2);
+//     ctx.lineTo(384.6, 34.3);
+//     ctx.lineTo(374.7, 54.1);
+//     ctx.closePath();
+//     ctx.stroke();
+//
+//     // layer1/Group
+//
+//     // layer1/Group/Path
+//     ctx.save();
+//     ctx.beginPath();
+//     ctx.moveTo(262.0, 53.8);
+//     ctx.lineTo(276.9, 42.1);
+//     ctx.lineTo(270.7, 29.8);
+//     ctx.bezierCurveTo(270.7, 29.8, 259.9, 36.8, 252.8, 43.0);
+//     ctx.lineTo(262.0, 53.8);
+//     ctx.closePath();
+//     ctx.stroke();
+//
+//     // layer1/Path
+//     ctx.restore();
+//     ctx.beginPath();
+//     ctx.moveTo(248.5, 64.5);
+//     ctx.lineTo(262.0, 53.8);
+//     ctx.stroke();
+//
+//     // layer1/Rectangle
+//     ctx.beginPath();
+//     ctx.moveTo(328.1, 46.2);
+//     ctx.lineTo(315.5, 39.9);
+//     ctx.lineTo(321.5, 27.8);
+//     ctx.lineTo(334.1, 34.1);
+//     ctx.lineTo(328.1, 46.2);
+//     ctx.closePath();
+//     ctx.stroke();
+//
+//     // layer1/Rectangle
+//     ctx.beginPath();
+//     ctx.moveTo(336.2, 50.4);
+//     ctx.lineTo(328.1, 46.3);
+//     ctx.lineTo(334.2, 34.2);
+//     ctx.lineTo(342.3, 38.3);
+//     ctx.lineTo(336.2, 50.4);
+//     ctx.closePath();
+//     ctx.stroke();
+//
+//     // layer1/Rectangle
+//     ctx.beginPath();
+//     ctx.moveTo(340.9, 52.7);
+//     ctx.lineTo(336.3, 50.4);
+//     ctx.lineTo(342.4, 38.1);
+//     ctx.lineTo(347.0, 40.5);
+//     ctx.lineTo(340.9, 52.7);
+//     ctx.closePath();
+//     ctx.stroke();
+//
+//     // layer1/Rectangle
+//     ctx.beginPath();
+//     ctx.moveTo(347.0, 55.7);
+//     ctx.lineTo(340.9, 52.7);
+//     ctx.lineTo(347.0, 40.4);
+//     ctx.lineTo(353.1, 43.5);
+//     ctx.lineTo(347.0, 55.7);
+//     ctx.closePath();
+//     ctx.stroke();
+//
+//     // layer1/Rectangle
+//     ctx.beginPath();
+//     ctx.moveTo(368.5, 66.5);
+//     ctx.lineTo(347.0, 55.7);
+//     ctx.lineTo(353.2, 43.3);
+//     ctx.lineTo(374.7, 54.1);
+//     ctx.lineTo(368.5, 66.5);
+//     ctx.closePath();
+//     ctx.stroke();
+//
+//     // layer1/N303
+//     ctx.font = "Bold 12.0px 'Lato'";
+//     ctx.fillStyle = "rgb(255, 255, 255)";
+//     ctx.fillText("N303", 31.2, 93.9);
+//     ctx.restore();
+//
+// };
